40660c44574c40de4f55e26de651fdafc61c4d88
Graham <graham.clift@am.sony.com>
2017-04-22 18:17:50 -0700

    Added fullscreen support

--- app/src/main/java/com/sony/tv/app/atsc3receiver1_0/PlayerActivity.java
+++ app/src/main/java/com/sony/tv/app/atsc3receiver1_0/PlayerActivity.java
@@ -230,6 +230,14 @@ public class PlayerActivity extends Activity implements OnClickListener, ExoPlay
       }
     };
     timerForChannelChange.schedule(timerTaskDown,10*60*1000);
+    getWindow().getDecorView().setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    
   }
 
 
